7b3ea480d87fb7ec6fae8860c91fce7ea7e863e7
isaacrael <grael@comcast.net>
2017-01-14 22:16:25 -0700

    "my first commit"

---
+++ showmodules.py
@@ -0,0 +1,15 @@
+__author__ = 'isaac'
+
+def bigger(a, b):
+    if a > b:
+        return a
+    elif a <b:
+        return b
+    else:
+        return a
+bigger(3,5)
+
+
+def timestwo(x):
+    return x*2
+timestwo(2)
